547fa1f2131d7eda38980be941c5575811f5e7b6
Antony Holmes <antony@antonybholmes.com>
2023-01-26 11:09:54 -0500

    Bug fixes and updates.

---
+++ src/content/config.ts
@@ -0,0 +1,27 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content"
+
+// 2. Define your collection(s)
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    authors: z.string().array(),
+    categories: z.string().array(),
+    hero: z.string(),
+    heroCaption: z.string().optional(),
+    tags: z.string().array(),
+    rating: z.number().optional(),
+    pros: z.string().array().optional(),
+    cons: z.string().array().optional(),
+    details: z.string().array().optional(),
+    url: z.string().optional(),
+    status: z.string().default("draft"),
+    type: z.string().default("post"),
+  }),
+})
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  blog: blogCollection,
+}
