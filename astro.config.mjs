cc0adeb857ce5aa09bac8ba618bbad9e9c0ce8a7
Antony Holmes <hello@antonyholmes.dev>
2023-01-28 11:52:05 -0500

    Bug fixes and updates.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from "astro/config" //import react from '@astrojs/react'; //import preact from '@astrojs/preact'
+import { defineConfig } from "astro/config"; //import react from '@astrojs/react'; //import preact from '@astrojs/preact'
 
-import robotsTxt from "astro-robots-txt"
-import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap" //import solid from "@astrojs/solid-js";
+import robotsTxt from "astro-robots-txt";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap"; //import solid from "@astrojs/solid-js";
 //import preact from "@astrojs/preact";
 // https://astro.build/config
 // https://astro.build/config
@@ -11,28 +11,30 @@ import sitemap from "@astrojs/sitemap" //import solid from "@astrojs/solid-js";
 //import react from "@astrojs/react";
 
 // https://astro.build/config
-import preact from "@astrojs/preact"
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch"
-import partytown from "@astrojs/partytown"
-import htmlBeautifier from "astro-html-beautifier"
+import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
+//import htmlBeautifier from "astro-html-beautifier";
+
+// https://astro.build/config
+import purgecss from "astro-purgecss";
+
+// https://astro.build/config
+import htmlMinifier from "astro-html-minifier";
+
+// https://astro.build/config
+//import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.antonyholmes.com",
   integrations: [
-    //svelte(),
-    preact(),
-    tailwind(),
-    sitemap(),
-    robotsTxt({
-      host: "antonyholmes.com",
-    }),
-    prefetch(),
-    partytown(),
-    htmlBeautifier(),
-  ],
+  //svelte(),
+  preact(), tailwind(), sitemap(), robotsTxt({
+    host: "antonyholmes.com"
+  }), prefetch(), partytown(), purgecss(), htmlMinifier()],
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
@@ -43,7 +45,7 @@ export default defineConfig({
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
       langs: [],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-  },
-})
+      wrap: true
+    }
+  }
+});
